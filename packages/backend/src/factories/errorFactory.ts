bddeafe97f546d5afbf0ad10e1493100c4d6bb11
Viktor Barna <therealvictorbrown@gmail.com>
2023-05-20 21:59:12 +0200

    feat: create BE config

---
+++ packages/backend/src/factories/errorFactory.ts
@@ -0,0 +1,57 @@
+type ErrorType = "NotFoundError" | "BadRequestError" | "InternalServerError";
+
+interface ErrorOptions {
+  message?: string;
+  code?: string;
+}
+
+class AppError {
+  code: string;
+  message: string;
+
+  constructor(message: string, code: string) {
+    this.message = message;
+    this.code = code;
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(message = "Resource not found") {
+    super(message, "NOT_FOUND_ERROR");
+  }
+}
+
+class BadRequestError extends AppError {
+  constructor(message = "Bad request") {
+    super(message, "BAD_REQUEST_ERROR");
+  }
+}
+
+class InternalServerError extends AppError {
+  constructor(message = "Internal server error") {
+    super(message, "INTERNAL_SERVER_ERROR");
+  }
+}
+
+function createError(type: ErrorType, options: ErrorOptions = {}): AppError {
+  const { message } = options;
+
+  switch (type) {
+    case "NotFoundError":
+      return new NotFoundError(message);
+    case "BadRequestError":
+      return new BadRequestError(message);
+    case "InternalServerError":
+      return new InternalServerError(message);
+    default:
+      return new InternalServerError("Unknown error");
+  }
+}
+
+export {
+  createError,
+  AppError,
+  NotFoundError,
+  BadRequestError,
+  InternalServerError,
+};

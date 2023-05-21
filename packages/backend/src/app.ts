827c82a5df215409c9cde93b88b3054528e49445
Viktor Barna <therealvictorbrown@gmail.com>
2023-05-21 22:09:36 +0200

    feat: directors endpoint

--- packages/backend/src/app.ts
+++ packages/backend/src/app.ts
@@ -8,6 +8,7 @@ import bodyParser from "body-parser";
 import apiKeyRouter from "./routes/apiKey.route";
 import genresRouter from "./routes/genres.route";
 import starsRouter from "./routes/stars.route";
+import directorsRouter from "./routes/directors.route";
 
 const app = express();
 const port = config.port;
@@ -27,6 +28,7 @@ app.get("/", (req: Request, res: Response) => {
 app.use("/apikey", apiKeyRouter);
 app.use("/genres", genresRouter);
 app.use("/stars", starsRouter);
+app.use("/directors", directorsRouter);
 
 app.use(errorHandler);
 

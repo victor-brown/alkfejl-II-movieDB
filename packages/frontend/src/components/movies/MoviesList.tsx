5b83289cf4f85216db2e09b40fe8ae9870ba73dc
Tompa Gábor <gabor.tompa@gmail.com>
2023-05-30 22:04:20 +0200

    feat: display data in movies list page

--- packages/frontend/src/components/movies/MoviesList.tsx
+++ packages/frontend/src/components/movies/MoviesList.tsx
@@ -5,37 +5,55 @@ import axios from 'axios'
 
 function MoviesList() {
 
-  const API = 'http://127.0.0.1:5555/movies'
-  const [list, setList] = useState([])
+    const API = 'http://127.0.0.1:5555/movies'
+    const [list, setList] = useState([])
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await axios.get(API)
-        setList(response.data)
-      } catch (error) {
-        console.log(error)
-      }
-    }
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await axios.get(API)
+                setList(response.data)
+            } catch (error) {
+                console.log(error)
+            }
+        }
 
-    fetchData()
-  }, []);
+        fetchData()
+    }, []);
 
-  return (
-    <TableContainer>
-      <Table variant='striped' colorScheme='gray'>
-        <TableCaption>Movies</TableCaption>
-        <Thead>
-          <Tr>
-          </Tr>
-        </Thead>
-        <Tbody>
+    return (
+        <TableContainer>
+            <Table variant='striped' colorScheme='gray'>
+                <TableCaption>Movies</TableCaption>
+                <Thead>
+                    <Tr>
+                        <Th>Id</Th>
+                        <Th>Title</Th>
+                        <Th>Year</Th>
+                        <Th>Functions</Th>
+                    </Tr>
+                </Thead>
+                <Tbody>
 
-        </Tbody>
-      </Table>
-    </TableContainer>
+                    {list &&
+                        list.map(({ id, title, year }) => (
+                            <Tr key={id}>
+                                <Td>{id}</Td>
+                                <Td>{title}</Td>
+                                <Td>{year}</Td>
+                                <Td>
+                                    <a href={`/movies/${id}`}>
+                                        view
+                                    </a>
+                                </Td>
+                            </Tr>
+                        ))}
 
-  )
+                </Tbody>
+            </Table>
+        </TableContainer>
+
+    )
 }
 
 export default MoviesList

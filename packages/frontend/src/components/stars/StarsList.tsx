3dec6c854509eba49f6e605e0a4537c0ceb719e2
Tompa Gábor <gabor.tompa@gmail.com>
2023-05-30 21:09:27 +0200

    feat: create link in Stars listing page to Star view.

--- packages/frontend/src/components/stars/StarsList.tsx
+++ packages/frontend/src/components/stars/StarsList.tsx
@@ -1,54 +1,59 @@
 import { TableContainer, Table, Thead, TableCaption, Tr, Th, Tbody, Td } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { Link } from 'react-router-dom'
 
 
 function StarsList() {
 
-    const API = 'http://127.0.0.1:5555/stars'
-    const [list, setList] = useState([])
-    
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(API)
-            setList(response.data)
-            console.log(response)
-          } catch (error) {
-            console.log(error)
-          }
-        }
-    
-        fetchData()
-      }, []);
+  const API = 'http://127.0.0.1:5555/stars'
+  const [list, setList] = useState([])
 
-    return (
-        <TableContainer>
-        <Table variant='striped' colorScheme='gray'>
-          <TableCaption>Movies</TableCaption>
-          <Thead>
-            <Tr>
-              <Th>Title</Th>
-              <Th>Star</Th>
-              <Th>Functions</Th>
-            </Tr>
-          </Thead>
-          <Tbody>
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API)
+        setList(response.data)
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-            {list &&
-                list.map(({ id, name, about }) => (
-                    <Tr>
-                        <Td>{ id }</Td>
-                        <Td>{ name }</Td>
-                        <Td>{ about }</Td>
-                    </Tr>
-                ))}
+    fetchData()
+  }, []);
 
-          </Tbody>
-        </Table>
-      </TableContainer>
+  return (
+    <TableContainer>
+      <Table variant='striped' colorScheme='gray'>
+        <TableCaption>Movies</TableCaption>
+        <Thead>
+          <Tr>
+            <Th>Title</Th>
+            <Th>Star</Th>
+            <Th>Functions</Th>
+          </Tr>
+        </Thead>
+        <Tbody>
 
-    )
+          {list &&
+            list.map(({ id, name }) => (
+              <Tr>
+                <Td>{id}</Td>
+                <Td>{name}</Td>
+                <Td>
+                  <a href={`/stars/${id}`}>
+                    view
+                  </a>
+                </Td>
+              </Tr>
+            ))}
+
+        </Tbody>
+      </Table>
+    </TableContainer>
+
+  )
 }
 
 export default StarsList

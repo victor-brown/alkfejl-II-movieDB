c1b0d45d986fb4b93701714b17218900841ed53e
Tompa Gábor <gabor.tompa@gmail.com>
2023-05-30 20:58:15 +0200

    feat: add star view

--- packages/frontend/src/components/stars/Star.tsx
+++ packages/frontend/src/components/stars/Star.tsx
@@ -1,32 +1,36 @@
 import { Card } from '@chakra-ui/react'
 import React, { useEffect, useState } from 'react'
 import axios from 'axios'
+import { useParams } from 'react-router-dom'
 
 
-function StarsList() {
+function Star() {
 
-    const API = 'http://127.0.0.1:5555/stars'
-    const [list, setList] = useState([])
-    
-    useEffect(() => {
-        const fetchData = async () => {
-          try {
-            const response = await axios.get(API)
-            setList(response.data)
-            console.log(response)
-          } catch (error) {
-            console.log(error)
-          }
-        }
-    
-        fetchData()
-      }, []);
+  const API = 'http://127.0.0.1:5555/stars/'
+  const [star, setStar] = useState([])
+  let param = useParams()
 
-    return (
-            <Card>
-            
-            <//Card>
-)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API + param.id)
+        setStar(response.data)
+        console.log(response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
+
+    fetchData()
+  }, []);
+
+  return (
+    <Card style={{ marginTop: "20px", gap: "5px", backgroundColor: "lightgrey" }}>
+      <h1>{ star.name }</h1>
+      <p>Id: { star.id }</p>
+      <p>About: { star.about }</p>
+    </Card>
+  )
 }
 
-export default StarsList
+export default Star

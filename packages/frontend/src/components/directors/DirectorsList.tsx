3bd605f52c03c7947ffb706ea97f469b37e10877
Tompa Gábor <gabor.tompa@gmail.com>
2023-05-31 21:35:42 +0200

    feat: add directorlist file

---
+++ packages/frontend/src/components/directors/DirectorsList.tsx
@@ -0,0 +1,9 @@
+import { TableContainer, Table, Thead, TableCaption, Tr, Th, Tbody, Td } from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+
+
+function DirectorsList() {
+}
+
+export default DirectorsList

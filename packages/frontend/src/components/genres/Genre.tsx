58f5b099dce4e62f25b1ccdaa293b1decfff53ba
Tompa Gábor <gabor.tompa@gmail.com>
2023-05-31 22:45:57 +0200

    feat: add genre view data

--- packages/frontend/src/components/genres/Genre.tsx
+++ packages/frontend/src/components/genres/Genre.tsx
@@ -6,28 +6,31 @@ import { useParams } from 'react-router-dom'
 
 function Genre() {
 
-    const API = 'http://127.0.0.1:5555/genres/'
-    const [genre, setGenre] = useState([])
-    let param = useParams()
+  const API = 'http://127.0.0.1:5555/genres/'
+  const [genre, setGenre] = useState([])
+  let param = useParams()
 
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.get(API + param.id)
-                setGenre(response.data)
-            } catch (error) {
-                console.log(error)
-            }
-        }
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(API + param.id)
+        setGenre(response.data)
+      } catch (error) {
+        console.log(error)
+      }
+    }
 
-        fetchData()
-    }, []);
+    fetchData()
+  }, []);
 
-    return (
-        <Card style={{ marginTop: "20px", gap: "5px", backgroundColor: "lightgrey" }}>
-
-        </Card>
-    )
+  return (
+    <Card style={{ marginTop: "20px", gap: "5px", backgroundColor: "lightgrey" }}>
+      <h1>{genre.name}</h1>
+      <div style={{ display: "flex", padding: "20px", gap: "5px" }}>
+        <p><b>Id:</b> {genre.id}</p>
+      </div>
+    </Card>
+  )
 }
 
 export default Genre
